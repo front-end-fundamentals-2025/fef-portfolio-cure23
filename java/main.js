b2f95f6a0361af9885e1dde1a787576408d81cbe
cure23 <cure23xv@student.ju.se>
2025-02-19 10:42:56 +0100

    Removed Commented - Submit Java
    - removed some commented out animations code
    - tried to fix submit re-loading page issue (needs improvement) with js

--- java/main.js
+++ java/main.js
@@ -1,9 +1,10 @@
-const submitButtonElement = document.getElementById("submitb");
+const submitButtonElement = document.getElementById("submit-button");
 const bodyElement = document.querySelector("body");
 
 const inputElement = document.querySelector("input");
 
 submitButtonElement.addEventListener("click", function (e) {
+  e.preventDefault(); // from https://stackoverflow.com/questions/19454310/stop-form-refreshing-page-on-submit
   bodyElement.style.backgroundColor = "#FF0000";
   let enteredName = inputElement.value;
 
