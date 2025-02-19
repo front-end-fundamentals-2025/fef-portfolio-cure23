13fca380423db0a17b5743f864ad682a0a668237
cure23 <cure23xv@student.ju.se>
2025-02-19 16:19:34 +0100

    Local Storage Submission Try2
    -still needs work

--- java/main.js
+++ java/main.js
@@ -4,13 +4,24 @@ const contactParElement = document.getElementById("contact-par");
 const nameInputElement = document.getElementById("name-input");
 const emailInputElement = document.getElementById("email-input");
 const messageInputElement = document.getElementById("message-input");
+const contactFooterElement = document.getElementById("contact-footer");
 const formElement = document.querySelector("form");
 const bodyElement = document.querySelector("body");
 
 const inputElement = document.querySelector("input");
 
+if (localStorage.submitted === true || !localStorage.submitted) {
+  bodyElement.style.backgroundColor = "#ffff00";
+} else {
+  bodyElement.style.backgroundColor = "#00ffff";
+}
 //store partial-input before submission
-localStorage.name = nameInputElement.value;
+/* let nameEnter = nameInputElement.value;
+
+if (nameEnter !== "") {
+  localStorage.text = nameEnter;
+  nameInputElement.value = localStorage.text;
+} */
 
 //nameInputElement.value = localStorage.enteredName;
 
@@ -23,15 +34,21 @@ submitButtonElement.addEventListener("click", function (e) {
 
   //Change message and colours if all input boxes in the form are filled out
   if (enteredName !== "" && enteredEmail !== "" && enteredMessage !== "") {
-    bodyElement.style.backgroundColor = "#acb6a8";
+    //bodyElement.style.backgroundColor = "#acb6a8";
     contactMeElement.style.color = "#FFFFFF";
     contactMeElement.innerText = "Thank You " + enteredName + "!";
     contactParElement.innerText =
       "I'll get back to you within 24 hours.\nLooking forward to working togrther";
 
+    //footer position
+    contactFooterElement.style.position = "absolute";
+    contactFooterElement.style.bottom = "0";
+
     //remove form after it was submitted
     formElement.remove();
+    localStorage.submitted = true;
   } else {
+    localStorage.submitted = false;
     // red border to the box that wasnt filled out
     if (enteredName == "") {
       nameInputElement.style.border = "1px solid #FF0000";
